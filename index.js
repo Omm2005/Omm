d914bf18884b40ddd9a187d7df3ebb700783a421
Omm2005 <109162009+Omm2005@users.noreply.github.com>
2022-09-24 14:33:37 +0530

    Uploaded new files

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".knowMore").hover(function() {
+    $(this).toggleClass("btn-light btn-dark")
+})
+
+$(".contact-btn").hover(function() {
+    $(this).toggleClass("btn-light btn-dark")
+    $(".btn-a").toggleClass("black")
+})
+
